fe23307819abbfa52579c794a40f996487c24abf
Mansi Patel <52688826+mansipatel241@users.noreply.github.com>
2022-04-17 12:05:56 +0530

    bindActionCreators with object destructering

--- src/Shop.js
+++ src/Shop.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import { actionCreators } from './state';
 
 const Shop = () => {
 
   const dispatch = useDispatch();
-
+  const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators, dispatch);
+  const balance = useSelector(state=> state.amount);
   return (
     <div className="my-2">
         <h2>Deposit/Withdraw Money</h2>
-        <button className="btn btn-primary mx-3" onClick={()=> dispatch(actionCreators.withdrawMoney(100))}>-</button>
-        Update Balance
-        <button className="btn btn-primary mx-3" onClick={()=> dispatch(actionCreators.depositMoney(100))}>+</button>
+        <button className="btn btn-primary mx-3" onClick={()=> withdrawMoney(100)}>-</button>
+        Update Balance : {balance}
+        <button className="btn btn-primary mx-3" onClick={()=> depositMoney(100)}>+</button>
     </div>
   )
 }
